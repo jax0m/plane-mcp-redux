9ae577321d01b1deb3ddbea7691b69ddc6dd8b26
jax0m <github.reword187@passinbox.com>
2026-04-13 18:15:32 +0000

    Updates for dev container to pre install some packages that are of use as well as to pre-position pre-commit for dev checks. Also installs npm which is a requirement for the pi-coding-agent I am using at least.

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,7 @@
+# apt update
+# apt install -y python3 python3-venv python3-pip pre-commit python-is-python3 dos2unix
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 24
